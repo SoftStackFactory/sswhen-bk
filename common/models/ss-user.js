3ce5886b9c4701c40e6ba6a740b1bf78b0331465
dcarandangssf <dcarandang@softstackfactory.org>
2018-02-21 07:30:59 +0000

    added hook to create token and login user after registering

--- common/models/ss-user.js
+++ common/models/ss-user.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Ssuser) {
-
+    Ssuser.observe('after save', function(ctx, next) {
+        if (ctx.isNewInstance === true) {
+            var instance = ctx.instance;
+            instance.createAccessToken(1209600000, function(err, response) {
+                if (err === null) ctx.instance["token"] = response.id;
+                next();
+            });
+        }
+        else {
+            next();
+        }
+    });
 };

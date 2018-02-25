216ae76814da1bc337a5f83a33497cd04f6155ca
dcarandangssf <dcarandang@softstackfactory.org>
2018-02-25 23:39:42 +0000

    changed datasource to mongodb instead of memory

--- common/models/ss-user.js
+++ common/models/ss-user.js
@@ -1,16 +1,22 @@
 'use strict';
 
 module.exports = function(Ssuser) {
+    
+    // hook that creates a token and logs in a new user
     Ssuser.observe('after save', function(ctx, next) {
         if (ctx.isNewInstance === true) {
             var instance = ctx.instance;
+            
             instance.createAccessToken(1209600000, function(err, response) {
-                if (err === null) ctx.instance["token"] = response.id;
-                next();
+                if (err === null) {
+                    ctx.instance["token"] = response.id;
+                    next();
+                }
             });
-        }
-        else {
+            
+        } else {
             next();
         }
     });
+    
 };
